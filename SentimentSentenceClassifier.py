a9d649a4c766407bbccb9dc61a0ce5ca4a3c11e3
xinan1911 <xinan1911@gmail.com>
2022-03-21 16:32:10 +0100

    new update

--- SentimentSentenceClassifier.py
+++ SentimentSentenceClassifier.py
@@ -7,15 +7,20 @@ import json
 import tensorflow as tf
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Embedding, GlobalAveragePooling1D, Dense
+#from tensorflow.keras.models import Sequential
+#from tensorflow.keras.layers import Embedding, GlobalAveragePooling1D, Dense
 import numpy as np
+
+#fix rubuild TensorFlow with te appropraite cmpiler flags error
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 
 
 #options to make
-training_size = 18700 #normally 70% of the dataset
-max_length = 30 # the maximal length of sequences of sentencea in the texts
-
+training_size = 19000 #normally 70% of the dataset
+max_length = 100 # the maximal length of sequences of sentence in the texts
+vocab_size = 20000
+embedding_dim = 16
 
 #import data
 # reference: rishabhmisra.github.io/publications/
@@ -24,6 +29,7 @@ def parse_data(file):
        yield json.loads(l)
 
 data = list(parse_data('Sarcasm_Headlines_Dataset.json'))
+
 
 sentences = []
 labels = []
@@ -41,7 +47,7 @@ labels_training = labels[0:training_size]
 labels_testing = labels[training_size:]
 
 #preprocessing on the texts
-tokenizer = Tokenizer(oov_token="<OOV>")
+tokenizer = Tokenizer(num_words=vocab_size, oov_token="<OOV>") #it is important to set num_words
 tokenizer.fit_on_texts(sentences_training)
 word_index = tokenizer.word_index
 
@@ -54,14 +60,35 @@ padded_testing = pad_sequences(sequences_testing, padding = 'post', maxlen=max_l
 print(padded_testing.shape)
 
 #neural network
-model = Sequential([
+model = tf.keras.Sequential([
     #top layer embedding: to get the embedded vectors for each word
-    Embedding(input_dim=vocab_size, output_dim=embedding_dim, input_length=max_length),
-    GlobalAveragePooling1D(),
+    tf.keras.layers.Embedding(vocab_size, embedding_dim, input_length=max_length),
+    tf.keras.layers.GlobalAveragePooling1D(),
     # Dense layer: output = activation(dot(input, kernel) + bias)
-    Dense(24, activation='relu'), #output arrays of shape(None, 24)
-    Dense(1, activation='sigmoid') #output arrays of shape(None, 1)
+    tf.keras.layers.Dense(24, activation='relu'), #output arrays of shape(None, 24)
+    tf.keras.layers.Dense(1, activation='sigmoid') #output arrays of shape(None, 1)
 ])
 model.compile(loss='binary_crossentropy', optimizer='adam',metrics=['accuracy'])
+model.summary()
 
-#
+
+#traing:fit the model to the data
+num_epochs = 30;
+#data need to be in the form of an array
+padded_training = np.array(padded_training)
+labels_training = np.array(labels_training)
+padded_testing = np.array(padded_testing)
+labels_testing = np.array(labels_testing)
+
+hisory = model.fit(padded_training, labels_training, epochs = num_epochs,
+                   validation_data=(padded_testing, labels_testing), verbose=2)
+
+sentence = [
+    "granny starting to fear spiders in the garden might be real",
+    "the weather today is bright and sunny",
+    "I don't want to go out with you",
+    "You are taller than a tree"]
+sequences = tokenizer.texts_to_sequences(sentence)
+
+padded = pad_sequences(sequences, maxlen=max_length, padding="post", truncating="post")
+print(model.predict(padded))
